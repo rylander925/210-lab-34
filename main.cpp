a26e0ad5235cbe1116fd31f5c2f5bae8fd85da6d
rylander925 <rylander925@gmail.com>
2025-11-16 14:16:47 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,36 @@
+/*
+COMSC-210 | Lab 34 | Rylan Der
+IDE Used: Visual Studio Code
+*/
+
+struct Edge {
+    int src, dest, weight;
+};
+
+typedef pair<int, int> Pair;  // Creates an alias 'Pair' for the pair<int,int> data type
+                              // so below we can use vector<Pair> rather than vector<pair<int, int>>
+class Graph {
+    public:
+    // a vector of vectors of Pairs to represent an adjacency list
+    vector<vector<Pair>> adjList;
+    // constructor
+    Graph(vector<Edge> const &edges) {
+        // resize the vector to hold SIZE elements of type vector<Edge>
+        adjList.resize(SIZE);
+        // add edges to the directed graph
+        for (auto &edge: edges) {
+            int src = edge.src;
+            int dest = edge.dest;
+            int weight = edge.weight;
+            adjList[src].push_back(make_pair(dest, weight));
+            adjList[dest].push_back(make_pair(src, weight));
+        }
+    }
+    void printGraph() {
+        for (int i = 0; i < adjList.size(); i++) {
+            for (Pair v: adjList[i])
+                cout << "(" << i << ", " << v.first << ", " << v.second << ") ";
+            cout << endl;
+        }
+    }
+};
